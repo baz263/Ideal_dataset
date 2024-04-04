d9361855e060e0fa566dd48cb9c0cba35edd497f
harbarbar <baz263@gmail.com>
2024-04-04 15:57:35 +0200

    .

--- day_consumption.py
+++ day_consumption.py
@@ -3,7 +3,7 @@ import seaborn as sns
 
 def day_consumption(df, homeid = None):
     fig, axs = plt.subplots(1,1, figsize = (10, 5))
-    order = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'holiday']
+    order = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
     sns.boxplot(data = df, x = df.day, y = 'electric-combined', ax=axs, order = order)
     plt.title(f'daily consumption for home {homeid}')
     #axs.get_yaxis().get_major_formatter().set_scientific(False)
