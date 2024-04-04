d6fa66872bc18e01df21e411409a59d633c2fc33
harbarbar <baz263@gmail.com>
2024-04-04 14:38:10 +0200

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,25 @@
 import streamlit as st
+import boto3
+import StringIO
+
+
 st.text('boob')
+
+session = boto3.Session(
+    aws_access_key_id=st.secrets('AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key=st.secrets('AWS_SECRET_ACCESS_KEY'))
+
+
+# Create an S3 resource object using the session
+s3 = session.resource('s3')
+
+obj = s3.Object('electric1hcsvs', '1H_csv/hourly_105.csv')
+response = obj.get()
+
+# The object's data is in the 'Body' field of the response
+data = response['Body'].read().decode('utf-8')
+
+# Use pandas to read the CSV data into a DataFrame
+df = pd.read_csv(StringIO(data))
+
+print(df)
