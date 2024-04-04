f811992caf7fafd9f1b927a6937dc3f1f9e08159
harbarbar <baz263@gmail.com>
2024-04-04 16:57:01 +0200

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ col1, col2 = st.columns([1,2], gap='large')
 
 with col1:
     fig1 = heatmap2(df)
-    fig1.update_layout(autosize=False, width=400)
+    fig1.update_layout(autosize=True)
     st.plotly_chart(fig1)
 
 
