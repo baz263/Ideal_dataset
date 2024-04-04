500dab46004566108a1647deac1637b1ec5049e2
harbarbar <baz263@gmail.com>
2024-04-04 16:08:06 +0200

    .

--- day_consumption.py
+++ day_consumption.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 def day_consumption_outliersremoved(df):
+    order = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday','Saturday', 'Sunday']
     fig, ax = plt.subplots()
 
     df = df['electric-combined'].copy().reset_index()
@@ -9,5 +10,5 @@ def day_consumption_outliersremoved(df):
     Q3 = df.quantile(0.75)
     IQR = Q3 - Q1
     df = df[(df > (Q1 - 1.5 * IQR)) & (df < (Q3 + 1.5 * IQR))]
-    sns.boxplot(data = df, x = df.time.dt.day_name(), y = 'electric-combined', ax=ax)
+    sns.boxplot(data = df, x = df.time.dt.day_name(), y = 'electric-combined', ax=ax, order = order)
     return fig
