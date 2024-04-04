136b172d47e3d2fda2cae4c854031f28ca65e1a1
harbarbar <baz263@gmail.com>
2024-04-04 16:18:22 +0200

    .

--- day_consumption.py
+++ day_consumption.py
@@ -9,6 +9,6 @@ def day_consumption_outliersremoved(df):
     Q1 = df.quantile(0.25)
     Q3 = df.quantile(0.75)
     IQR = Q3 - Q1
-    df = df[(df > (Q1 - 1.5 * IQR)) & (df < (Q3 + 1.5 * IQR))]
+    #df = df[(df > (Q1 - 1.5 * IQR)) & (df < (Q3 + 1.5 * IQR))]
     sns.boxplot(data = df, x = df.time.dt.day_name(), y = 'electric-combined', ax=ax, order = order)
     return fig
