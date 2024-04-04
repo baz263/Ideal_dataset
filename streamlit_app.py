a8a1f13ae456e146a1ac6b68f5c65fe44a817a4c
harbarbar <baz263@gmail.com>
2024-04-04 14:46:25 +0200

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,23 +3,23 @@ import boto3
 from io import StringIO
 
 
-st.text('boob')
+st.write('boob')
+def df_getter():
+    session = boto3.Session(
+        aws_access_key_id=st.secrets('AWS_ACCESS_KEY_ID'),
+        aws_secret_access_key=st.secrets('AWS_SECRET_ACCESS_KEY'))
 
-session = boto3.Session(
-    aws_access_key_id=st.secrets('AWS_ACCESS_KEY_ID'),
-    aws_secret_access_key=st.secrets('AWS_SECRET_ACCESS_KEY'))
 
+    # Create an S3 resource object using the session
+    s3 = session.resource('s3')
 
-# Create an S3 resource object using the session
-s3 = session.resource('s3')
+    obj = s3.Object('electric1hcsvs', '1H_csv/hourly_105.csv')
+    response = obj.get()
 
-obj = s3.Object('electric1hcsvs', '1H_csv/hourly_105.csv')
-response = obj.get()
+    # The object's data is in the 'Body' field of the response
+    data = response['Body'].read().decode('utf-8')
 
-# The object's data is in the 'Body' field of the response
-data = response['Body'].read().decode('utf-8')
-
-# Use pandas to read the CSV data into a DataFrame
-df = pd.read_csv(StringIO(data))
+    # Use pandas to read the CSV data into a DataFrame
+    df = pd.read_csv(StringIO(data))
 
 st.dataframe(df)
