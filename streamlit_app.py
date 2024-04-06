139ce621f8ffc4d5accf6b2de0faadcc2a36ab91
harbarbar <baz263@gmail.com>
2024-04-06 19:05:00 +0200

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -197,6 +197,7 @@ with tab1:
 
 with tab2:
     fbprophet_dataframe = model_maker()
+    st.write(fbprophet_dataframe)
     #st.dataframe(fbprophet_dataframe)
     merged_df = pd.merge(df_1h_all, fbprophet_dataframe, how='outer', left_index=True, right_on='ds')
     #st.dataframe(merged_df)
