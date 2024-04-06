8f0840a830c22ca31d73b22f5bc24ff509c44986
harbarbar <baz263@gmail.com>
2024-04-06 19:19:06 +0200

    .

--- fbprophet_plot.py
+++ fbprophet_plot.py
@@ -2,9 +2,9 @@ import plotly.graph_objects as go
 
 def fbprophet_plot(df):
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=df.ds, y=df['yhat'], mode='lines', name='yhat'))
+    fig.add_trace(go.Scatter(x=df.time, y=df['yhat'], mode='lines', name='yhat'))
     #fig.add_trace(go.Scatter(x=df.ds, y=df['yhat_lower'], fill=None, mode='lines', line_color='rgba(255,0,0,0.2)', name='yhat_lower'))
     #fig.add_trace(go.Scatter(x=df.ds, y=df['yhat_upper'], fill='tonexty', mode='lines', line_color='rgba(255,0,0,0.2)', name='yhat_upper'))
-    fig.add_trace(go.Scatter(x=df.ds, y=df['electric-combined'], mode='lines', name='electric-combined'))
+    fig.add_trace(go.Scatter(x=df.time, y=df['electric-combined'], mode='lines', name='electric-combined'))
     return fig
 
