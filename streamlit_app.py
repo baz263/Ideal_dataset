06518bc6c501058f742a229d20cba9a5cea8917c
harbarbar <baz263@gmail.com>
2024-04-11 11:52:57 +0200

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -312,10 +312,10 @@ with tab1:
     col1, col2 = st.columns([3,2])
 
     with col2:
-        fig1 = heatmap2(df2)
-        fig1.update_layout(autosize = True)
+        #fig1 = heatmap2(df2)
+        #fig1.update_layout(autosize = True)
 
-        st.plotly_chart(fig1 ,use_container_width =True)
+        #st.plotly_chart(fig1 ,use_container_width =True)
 
 
     with col1:
